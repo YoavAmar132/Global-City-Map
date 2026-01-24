5afbe73745dd29e9678135e4fd6fd9ef353b86ab
yuval hayat <yuval.chayat1@gmail.com>
2026-01-24 23:11:19 +0200

    deleted unnecessary files

--- client/src/main/java/gcm/client/controllers/WelcomeController.java
+++ client/src/main/java/gcm/client/controllers/WelcomeController.java
@@ -1,7 +1,6 @@
 package gcm.client.controllers;
 
 import gcm.client.controllers.catalogPublic.GuestCatalogController;
-import gcm.client.controllers.user_util.CatalogController;
 import gcm.client.utill.ClientApp;
 import javafx.fxml.FXML;
 
@@ -9,7 +8,7 @@ public class WelcomeController {
 
     @FXML
     private void onViewCatalog() {
-        ClientApp.getNavigator().show(CatalogController.class);
+        ClientApp.getNavigator().show(GuestCatalogController.class);
     }
 
     @FXML
